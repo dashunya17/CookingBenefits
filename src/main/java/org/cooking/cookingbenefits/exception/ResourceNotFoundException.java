6bda4751019db11efecbc908a0cf10d6b856c02e
Dasha <d.gmail.com>
2026-02-11 22:48:05 +0300

    7.2

--- src/main/java/org/cooking/cookingbenefits/exception/ResourceNotFoundException.java
+++ src/main/java/org/cooking/cookingbenefits/exception/ResourceNotFoundException.java
@@ -1,8 +1,11 @@
 package org.cooking.cookingbenefits.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
-@ControllerAdvice
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
